602946c86b374dc659ebfc8f6512a78f37996c74
Logan Thirion <thirionlogan@gmail.com>
2025-07-10 04:05:44 -0500

    Update ESLint configuration to include global ignores and improve file organization. Added globalIgnores for 'dist/**' and structured file settings for better clarity in linting rules.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,20 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
-
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.node } },
+  globalIgnores(['dist/**']),
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+
+    languageOptions: { globals: globals.node },
+  },
   tseslint.configs.recommended,
 ]);
